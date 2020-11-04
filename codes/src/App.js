2444ecdfd069d118beec49bd3902c7f4f19095ab
paikwiki <paikwiki@gmail.com>
2020-11-04 18:27:58 +0900

    Create a tiny sample of styled-component(for the test)

--- codes/src/App.js
+++ codes/src/App.js
@@ -2,11 +2,13 @@ import './scss/App.scss';
 import tw from 'twin.macro';
 
 const CustomH1 = tw.h1`text-green-500 text-2xl`;
+const Card = tw.div`p-4 shadow-lg max-w-md border-2 bg-white`;
 
 function App() {
   return (
     <div className="App">
       <CustomH1>Hello CRA!</CustomH1>
+      <Card>I am a Card.</Card>
     </div>
   );
 }
